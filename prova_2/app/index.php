4e6039190d34cdb62b6e0caa980516c148a09041
R3TR0R0C4 <nicoroca04@gmail.com>
2024-10-09 20:00:41 +0200

    ?

---
+++ prova_2/app/index.php
@@ -0,0 +1,64 @@
+<?php
+$var1 = 4;
+$var2 = 5;
+
+$var3 = $var1 + $var2;
+
+$books=[
+    [
+        "name"=>"Your Name",
+        "author"=>"Makoto Shinkai",
+        "releaseYear"=>"2016"
+    ],
+    [
+        "name"=>"Weathering with You",
+        "author"=>"Makoto Shinkai",
+        "releaseYear"=>"2019"
+    ],
+    [
+        "name"=>"Insomniacs after School",
+        "author"=>"Makoto Ojiro",
+        "releaseYear"=>"2019"
+    ],
+    [
+        "name"=>"Shingeki no Kyojin",
+        "author"=>"Hajime Isayama",
+        "releaseYear"=>"2009"
+    ],
+    [
+        "name"=>"Death Note",
+        "author"=>"Tsugumi Ohba",
+        "releaseYear"=>"2003"
+    ],
+];
+
+//        $books=["Your Name","Weathering with You","Insomniacs after School","Shingeki no Kyojin","Death Note"];
+
+function filteredAuthhors($books, $author) :array {
+    //Creem array
+    $filteredAuthors= [];
+    foreach ($books as $book){
+        if ($book['author']===$author){
+            $filteredAuthors[] = $book;
+        }
+    }
+    return $filteredAuthors;
+}
+function filteredYears($books, $year) :array {
+    //Creem array
+    $filteredYears= [];
+    foreach ($books as $book){
+        if ($book['releaseYear']>$year){
+            $filteredYears[] = $book;
+        }
+    }
+    return $filteredYears;
+}
+
+//funcio lambda
+$filteredBooks = array_filter($books, function ($book) {
+    return $book["releaseYear"] >= 2003 && $book["releaseYear"] <= 2016;
+});
+// var_dump($books);
+?>
+
