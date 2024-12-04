3a3799a87e1aaf5c20e250bbed71f7b612094623
R3TR0R0C4 <nicoroca04@gmail.com>
2024-12-04 14:19:54 +0100

    blank_start

--- routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-//fitxer per definir les rutes
-return [
-    '/' => '../app/index.php',
-    '/home' => '../app/index.php',
-    '/index' => '../app/index.php',
-    '/index.php' => '../app/index.php',
-];
